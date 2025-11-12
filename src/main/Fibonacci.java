36f099deb960ddcf4552c00e51a762da24a79414
rvyas84 <rajanvyas84@gmail.com>
2025-11-11 21:13:51 -0800

    fibo

---
+++ src/main/Fibonacci.java
@@ -0,0 +1,28 @@
+public class Fibonacci {
+    public void calFibo(int num) {
+        int num1 = 0;
+        int num2 = 1;
+
+        for(int i = 1; i <= num; i++) {
+            System.out.println(num1 + " ");
+            int result = num1 + num2;
+            num1 = num2;
+            num2 = result;
+        }
+    }
+
+    public long calFiboRecursive(int num) {
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+        return calFiboRecursive(num - 1) + calFiboRecursive(num - 2);
+    }
+
+    public static void main(String[] args) {
+        Fibonacci fibo = new Fibonacci();
+        fibo.calFibo(10);
+
+        for (int i = 0; i < 11; i++) {
+            System.out.print(fibo.calFiboRecursive(i) + " ");
+        }
+    }
+}
